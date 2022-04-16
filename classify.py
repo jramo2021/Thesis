Made my own metrics. Made rotation fixed amount. Experimented with structures a bit

classify.py
@@ -36,21 +36,96 @@ def train(train_ds,val_ds):
     #     tf.keras.layers.Dense(num_classes)
     # ])
 
+    # '''Create model (Mod 2)'''
+    # model = tf.keras.Sequential([
+    #     tf.keras.layers.Rescaling(1./255),
+    #     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    #     tf.keras.layers.MaxPooling2D(),
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(256, activation='relu'),
+    #     tf.keras.layers.Dense(num_classes)
+    # ])
+
+    # '''Create model (Mod 3)'''
+    # model = tf.keras.Sequential([
+    #     tf.keras.layers.Rescaling(1./255),
+    #     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    #     tf.keras.layers.MaxPooling2D(),
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(128, activation='relu'),
+    #     tf.keras.layers.Dense(num_classes)
+    # ])
+
+    # '''Create model (Mod 4)'''
+    # # Maxed out memory after 5 runs
+    # model = tf.keras.Sequential([
+    #     tf.keras.layers.Rescaling(1./255),
+    #     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    #     tf.keras.layers.MaxPooling2D(),
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(1024, activation='relu'),
+    #     tf.keras.layers.Dense(num_classes)
+    # ])
+    
+    # '''Create model (Mod 5)'''
+    # # Maxed out memory after 16 runs
+    # model = tf.keras.Sequential([
+    #     tf.keras.layers.Rescaling(1./255),
+    #     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    #     tf.keras.layers.MaxPooling2D(),
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(512, activation='relu'),
+    #     tf.keras.layers.Dense(num_classes)
+    # ])
+    
+
+    # mergedOut = tf.keras.layers.Add()([model0.output,model1.output])
+    # model = tf.keras.layers.Model([model0.input,model1.input], mergedOut)
+
     # # ResNet and KGG16 notes https://keras.io/api/applications/
     # model = tf.keras.Sequential([
     #     tf.keras.applications.ResNet50(
     #         include_top=False,
     #         weights="imagenet",
     #         # input_tensor=None,
-    #         # input_shape=None,
+    #         input_shape=(256,256,3),
     #         # pooling=None,
     #         classes=num_classes),
+    #     tf.keras.layers.Dense(128, activation='relu'),
     #     tf.keras.layers.Dense(num_classes)])
 
+    # # Home made VGG16 https://towardsdatascience.com/step-by-step-vgg16-implementation-in-keras-for-beginners-a833c686ae6c
+    # model = tf.keras.Sequential()
+    # model.add(tf.keras.layers.Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    # model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    # model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
 
+    # model.add(tf.keras.layers.Flatten())
+    # model.add(tf.keras.layers.Dense(units=4096,activation="relu"))
+    # model.add(tf.keras.layers.Dense(units=4096,activation="relu"))
+    # model.add(tf.keras.layers.Dense(units=2, activation="softmax"))
+    
+    
+    
+    
     # model = VGG16(weights='imagenet', include_top=False)
 
-    # To view the training and validation accuracy for each training epock, 
+    # To view the training and validation accuracy for each training epoch, 
     # pass the metrics argument to model.compile
     # print("\nCompiling Model")
     
@@ -101,9 +176,11 @@ def train(train_ds,val_ds):
     # )
 
     end = time.time()
-    print("\nTraining took:%0.2f" %(end - start),"seconds\n")
+    
+    training_time = end-start
+    print("\nTraining took:%0.2f" %training_time,"seconds\n")
 
-    return model, history
+    return model, history, training_time
     
 
     

data_management.py
@@ -52,7 +52,7 @@ def preprocess_data(data_dir, color_mode = 'rgb',aug_split = 0):
     '''load and preprocess an image dataset using Keras preprocessing layers and utilities'''
     '''Create a dataset'''
     # Typically standard size for smaller data sets (~1000 samples)
-    start = time.time()
+    
     batch_size = 32 
     # img_size = 512
     img_size = 256
@@ -64,8 +64,8 @@ def preprocess_data(data_dir, color_mode = 'rgb',aug_split = 0):
         # color_mode="grayscale",
         color_mode = color_mode,
         seed=123,
-        image_size=(img_size, img_size),
-        # image_size=(700, 460),
+        # image_size=(img_size, img_size),
+        image_size=(700, 460),
         batch_size=batch_size,
         shuffle=True)
 
@@ -75,14 +75,17 @@ def preprocess_data(data_dir, color_mode = 'rgb',aug_split = 0):
     
     # If augmentation split was valid, it will perform the augmentation 
     # and append the augmented data to the training set
-    print("Performing Data Augmentation:",aug_ds is not None)
+    print("\nPerforming Data Augmentation:",aug_ds is not None)
     if aug_ds is not None:
         train_ds = augment_data(train_ds,aug_ds)
-
-    print('\n#Total Batches:',len(train_ds) + len(val_ds) + len(test_ds))
+        print('After Adding Augmentation:')
+    
+    print('#Total Batches:',len(train_ds) + len(val_ds) + len(test_ds))
     print('#Training Batches:',len(train_ds))
     print('#Valdiation Batches:',len(val_ds))
     print('#Testing Batches:',len(test_ds),'\n')
+
+    
     
     '''Configure the dataset for performance'''
     AUTOTUNE = tf.data.AUTOTUNE
@@ -118,7 +121,8 @@ def preprocess_data(data_dir, color_mode = 'rgb',aug_split = 0):
 
 #     return train_ds, val_ds, test_ds, aug_ds
 
-'''Experimental: check that it works (total size is too big I think, maybe divide by total size?)'''
+
+
 def get_dataset_partitions_tf(ds, ds_size, train_split=0.8, aug_split = 0, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
     print(train_split + test_split + val_split)
     assert (train_split + test_split + val_split) == 1
@@ -153,7 +157,6 @@ def get_dataset_partitions_tf(ds, ds_size, train_split=0.8, aug_split = 0, val_s
     else:
         aug_ds = None
     
-
     return train_ds, val_ds, test_ds, aug_ds
 
 def augment_data(train_ds,aug_ds):
@@ -165,7 +168,7 @@ def augment_data(train_ds,aug_ds):
     # Performs random rotation and flip
     rot_and_flip_aug = tf.keras.Sequential([
         tf.keras.layers.RandomFlip("horizontal_and_vertical",seed=123),
-        tf.keras.layers.RandomRotation(factor=(-.5, .5),seed=123),
+        tf.keras.layers.RandomRotation(factor=(.35,.35),seed=123),
     ])
 
     # Applies rotation and flip to the data
@@ -176,30 +179,29 @@ def augment_data(train_ds,aug_ds):
     aug_ds = aug_ds.map(
         lambda x, y: (rot_and_flip_aug(x, training=True), y),num_parallel_calls=AUTOTUNE)
 
-
-    images, label = next(iter(train_ds))
-    print('\nBefore Concatenation')
-    print("number of training batches:",len(train_ds))
-    print('Batch_size:',len(images))
-    print('Rows:',len(images[0]))
-    print('Columns:',len(images[0][0]))
-    print('RGB:',len(images[0][0][0]))
+    # images, label = next(iter(train_ds))
+    # print('\nBefore Concatenation')
+    # print("number of training batches:",len(train_ds))
+    # print('Batch_size:',len(images))
+    # print('Rows:',len(images[0]))
+    # print('Columns:',len(images[0][0]))
+    # print('RGB:',len(images[0][0][0]))
 
     # Append the augmented data to the training data
     train_ds = train_ds.concatenate(aug_ds)
     
-    print('\nAfter Concatenation')
-    images, label = next(iter(train_ds))
-    print("number of training batches:",len(train_ds))
-    print('Batch_size:',len(images))
-    print('Rows:',len(images[0]))
-    print('Columns:',len(images[0][0]))
-    print('RGB:',len(images[0][0][0]))
+    # print('\nAfter Concatenation')
+    # images, label = next(iter(train_ds))
+    # print("number of training batches:",len(train_ds))
+    # print('Batch_size:',len(images))
+    # print('Rows:',len(images[0]))
+    # print('Columns:',len(images[0][0]))
+    # print('RGB:',len(images[0][0][0]))
     
     # train_ds = train_ds.shuffle(shuffle_size, seed = 123)
     
     # Show some augmentations
-    visualize_augmentations(train_ds,filename = "augmentations")
+    visualize_augmentations(train_ds,rot_and_flip_aug,filename = "augmentations")
 
     print(len(train_ds))
     return train_ds

main.py
@@ -1,3 +1,4 @@
+# from turtle import TPen
 from support_functions import *
 from data_management import *
 from classify import *
@@ -5,79 +6,197 @@ import pandas as pd
 
 
 def main():
+    
+    # Start Timer
     total_start = time.time()
-    # Download and Augment Data
+
+    '''Collect and Preprocess Data'''
+    # Download Data
     data_dir = get_data()
-    train_ds, val_ds, test_ds = preprocess_data(data_dir)
-    results = []
-    print("\nEvaluating Model")
     
-    total_ds = len(train_ds) + len(val_ds) + len(test_ds)
-    print("train size:",len(train_ds))
-    print("val size:",len(val_ds))
-    print("test size:",len(test_ds))
-    print("total size:",total_ds)
-
+    # Preprocess Data (Data Augmentation and datasplit splits)
+    train_ds, val_ds, test_ds = preprocess_data(data_dir,aug_split = 0)
+    
+    '''Define, Train and Evaluate the Model'''
     # Test 30 times because it is a stochastic process
-    # for i in range(0,30):
-    for i in range(1):
-        print("\n_______Test",i,"_______")
-        start = time.time()
-        
-        # Create and train the model using augmented data 
-        model, history = train(train_ds, val_ds)
-        
-        results.append(model.evaluate(test_ds,batch_size=32))
-        # results.append(model.predict(test_ds,batch_size=32))
-        end = time.time()
-        results[i].append(end-start)
-        
-    print(results)
-    print('\n',history.history['loss'])
-    plt.figure()
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
-    plt.savefig('/home/Thesis/Loss Plot.png')
-    
-    plt.figure()
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
-    plt.savefig('/home/Thesis/Accuracy Plot.png')
-    
-    '''Using Accuracy and MAE for Metrics'''
-    df = pd.DataFrame(results, columns = ["loss", "accuracy","mae","time"])
+    results = []
+    for i in range(50):
+        print("\n_______Test",i+1,"_______")
+        # Define and train the model 
+        model, history, training_time = train(train_ds, val_ds)
 
-    # Calculate Mean and Standard Deviation for Performance Metrics
-    mean_loss = df["loss"].mean()
-    mean_accuracy = df["accuracy"].mean()
-    mean_mae = df["mae"].mean()
-    mean_time = df["time"].mean()
-    std_loss = df["loss"].std()
-    std_accuracy = df["accuracy"].std()
-    std_mae = df["mae"].std()
-    std_time = df["time"].std()
+        
+        
+    #     # Evaluate the Model (classify test dataset)
+    #     results.append(model.evaluate(test_ds,batch_size=32))
 
+    #     # Append the time it took to train the model to the results
+    #     results[i].append(training_time)
+    
+    # # Displays true labels compared to estimated labels for 32 samples. 
+    # confirm_output(model, test_ds)
+    
+        # Calculates TP, TN, FP, FN and total accuracy
+        results.append(calculate_metrics(model,test_ds))
+        results[i].append(training_time)
+    
+    display_results(results)
+
+    # Plots the Loss and Accuracy over number of epochs
+    # plot_history(history)
+    
+    
+#     '''Using Accuracy and MAE for Metrics'''
+#     df = pd.DataFrame(results, columns = ["loss", "accuracy","mae","time"])
+    
+    
+#     # Calculate Mean and Standard Deviation for Performance Metrics
+#     mean_loss = df["loss"].mean()
+#     mean_accuracy = df["accuracy"].mean()
+#     mean_mae = df["mae"].mean()
+#     mean_time = df["time"].mean()
+#     std_loss = df["loss"].std()
+#     std_accuracy = df["accuracy"].std()
+#     std_mae = df["mae"].std()
+#     std_time = df["time"].std()
+
+# # df = pd.DataFrame(results, columns = ["loss", "accuracy","mae","est","time"])
+
+# #     # Calculate Mean and Standard Deviation for Performance Metrics
+# #     mean_loss = df["loss"].mean()
+# #     mean_accuracy = df["accuracy"].mean()
+# #     mean_mae = df["mae"].mean()
+# #     mean_est = df["est"].mean()
+# #     mean_time = df["time"].mean()
+# #     std_loss = df["loss"].std()
+# #     std_accuracy = df["accuracy"].std()
+# #     std_mae = df["mae"].std()
+# #     std_est = df["est"].std()
+# #     std_time = df["time"].std()
+
+#     total_end = time.time()
+
+#     # Display Results
+#     print("\nFinal Results:")
+#     print('Loss: (%0.2f' % (mean_loss*100),u'\u00b1','%0.2f' % (std_loss*100)+')%')
+#     print('Accuracy: (%0.2f' % (mean_accuracy*100),u'\u00b1','%0.2f' % (std_accuracy*100)+')%')
+#     # print('Estimated Accuracy: (%0.2f' % (mean_est*100),u'\u00b1','%0.2f' % (std_est*100)+')%')
+#     print('Mean Absolute Error: %0.2f' % (mean_mae),u'\u00b1','%0.2f' % (std_mae))
+#     print('Avg Time: (%0.2f' % (mean_time),u'\u00b1','%0.2f' % (std_time)+') seconds')
     total_end = time.time()
+    print('Total Time: %0.2f' % (total_end - total_start),"seconds")
+
+# def confirm_output(model, test_ds):
+#     '''Confirms that the model predicts reasonable labels.
+#     Uses the trained model to predict the labels of the first batch (32 samples) of test data.
+#     An example of the predicted label will be displayed alongside the true labels for visual confirmation.
+#     Since the evaluation and prediction methods have some variance, they are tested 30 times to show that 
+#     they have similar accuracy distributions'''
+#     mini_ds = test_ds.take(1)
+#     results = []
+#     for i in range(30):
+#         results.append(model.evaluate(mini_ds,batch_size=32))
+    
+#     mini_ds, labels = next(iter(test_ds))
+#     labels = tf.constant(labels).numpy()
+
+
+#     prediction = model.predict(mini_ds,batch_size=32)
+#     prediction_labels = prediction.argmax(axis=-1)
+#     print(prediction_labels)
+
+#     est = (1-sum(abs(prediction_labels-labels))/len(prediction_labels))
+        
+
+#     df = pd.DataFrame(results, columns = ["loss", "accuracy","mae"])
+    
+#     print('\nTrue Labels\n',labels)
+#     print('Predicted Labels\n',prediction_labels)
+
+#     mean_accuracy = df["accuracy"].mean()
+#     std_accuracy = df["accuracy"].std()
+#     print('Prediction Accuracy: %0.2f' % (est*100)+'%')
+#     print('Evaluation Accuracy: (%0.2f' % (mean_accuracy*100),u'\u00b1','%0.2f' % (std_accuracy*100)+')%')
+
+'''Calculates Accuracy of for the model classifying the test data'''
+def calculate_metrics(model,test_ds):
+    
+    # Initialize Lists
+    labels = []
+    prediction_labels = []
+    
+    # Make test dataset iterable object
+    ds = iter(test_ds)
+
+    # For each batch, append labels to both the true label array and predicted label array
+    for i in range(len(test_ds)):
+        
+        # Collect True Data Labels
+        images, temp_labels = next(ds)
+        temp_labels = tf.constant(temp_labels).numpy()
+        labels.extend(temp_labels)
+        
+        # Collect Prediction Labels
+        prediction = model.predict(images,batch_size=32)
+        temp_labels = prediction.argmax(axis=-1)
+        prediction_labels.extend(temp_labels)
+    
+    # Calculate True/False Positive/Negative
+    # Initialize count
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    total = len(labels)
+    for i in range(total):
+        if labels[i] == 1 and prediction_labels[i] == 1:
+            TP += 1     # True Positive
+        elif labels[i] == 0 and prediction_labels[i] == 0:
+            TN += 1     # True Negative
+        elif labels[i] == 0 and prediction_labels[i] == 1:
+            FP += 1     # False Positive
+        elif labels[i] == 1 and prediction_labels[i] == 0:
+            FN += 1     # False Negative
+        else:   # If labels aren't binary, raise error
+            raise ValueError("Labels are not binary")
+
+    # Calculate Accuracy and return results
+    accuracy = (TN + TP)/total
+    return [accuracy, TP/total, TN/total, FP/total, FN/total]
+    
+
+def display_results(results):
+    '''Using Accuracy and MAE for Metrics'''
+    df = pd.DataFrame(results, columns = ["accuracy","TP","TN","FP","FN","training_time"])
+    
+    
+    # Calculate Mean for Performance Metrics
+    mean_accuracy = df["accuracy"].mean()*100
+    mean_TP = df["TP"].mean()*100
+    mean_TN = df["TN"].mean()*100
+    mean_FP = df["FP"].mean()*100
+    mean_FN = df["FN"].mean()*100
+    mean_time = df["training_time"].mean()
+
+    # Calculate Standard Deviation for Performance Metrics
+    std_accuracy = df["accuracy"].std()*100
+    std_TP = df["TP"].std()*100
+    std_TN = df["TN"].std()*100
+    std_FP = df["FP"].std()*100
+    std_FN = df["FN"].std()*100
+    std_time = df["training_time"].std()
+
 
     # Display Results
     print("\nFinal Results:")
-    print('Loss: (%0.2f' % (mean_loss*100),u'\u00b1','%0.2f' % (std_loss*100)+')%')
-    print('Accuracy: (%0.2f' % (mean_accuracy*100),u'\u00b1','%0.2f' % (std_accuracy*100)+')%')
-    print('Mean Absolute Error: %0.2f' % (mean_mae),u'\u00b1','%0.2f' % (std_mae))
-    print('Avg Time: (%0.2f' % (mean_time),u'\u00b1','%0.2f' % (std_time)+') seconds')
-    print('Total Time: %0.2f' % (total_end - total_start),"seconds")
-
-    
+    print('Accuracy: (%0.2f' % (mean_accuracy),u'\u00b1','%0.2f' % (std_accuracy)+')%')
+    print('True Positive: (%0.2f' % (mean_TP),u'\u00b1','%0.2f' % (std_TP)+')%')
+    print('True Negative: (%0.2f' % (mean_TN),u'\u00b1','%0.2f' % (std_TN)+')%')
+    print('False Positive: (%0.2f' % (mean_FP),u'\u00b1','%0.2f' % (std_FP)+')%')
+    print('False Negative: (%0.2f' % (mean_FN),u'\u00b1','%0.2f' % (std_FN)+')%')
+    print('Avg Training Time: (%0.2f' % (mean_time),u'\u00b1','%0.2f' % (std_time)+') seconds')
+        
 
 if __name__ == '__main__': 
     main()

support_functions.py
@@ -19,7 +19,6 @@ def tree_file_printer(root):
             print (os.path.join(root, f))
 
 # tree_directory_printer('C:/Users/Ross/OneDrive/Documents/Thesis')
-
 '''Visualizations'''
 def visualize_sample_data(class_names,train_ds,filename = "visualize_sample_data"):
     # Save sample images of training data
@@ -49,15 +48,10 @@ def visualize_sample_data(class_names,train_ds,filename = "visualize_sample_data
 #     plt.savefig('/home/Thesis/'+ filename +'.png')
 
 
-def visualize_augmentations(train_ds,filename = "augmentations"):
+def visualize_augmentations(train_ds,rot_and_flip_aug,filename = "augmentations"):
     # Save sample images of training data
     plt.figure(figsize=(10, 10))
     image, label = next(iter(train_ds))
-
-    rot_and_flip_aug = tf.keras.Sequential([
-        tf.keras.layers.RandomFlip("horizontal_and_vertical",seed=123),
-        tf.keras.layers.RandomRotation(factor=(-.5, .5),seed=123),
-    ])
 
     ax = plt.subplot(3, 3, 1)
     plt.imshow(image[1].numpy().astype("uint8"))
@@ -70,17 +64,30 @@ def visualize_augmentations(train_ds,filename = "augmentations"):
         
         ax = plt.subplot(3, 3, i + 2)
         plt.imshow(result.numpy().astype("uint8"))
-        plt.title("Possible Augment")
+        plt.title('Possible Augment '+str(i+1))
         plt.axis("off")
 
     # Save image
     plt.savefig('/home/Thesis/'+filename+'.png')
 
-# def getSamplesFromDataGen(resultData):
-#     x = resultData.next() #fetch the first batch
-#     a = x[0] # train data
-#     b = x[1] # train label
-#     for i in range(0,5):
-#         plt.imshow(a[i])
-#         plt.title(b[i])
-#         plt.show() 
+def plot_history(history):
+
+    plt.figure() 
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    plt.savefig('/home/Thesis/Loss Plot.png')
+    
+    plt.figure()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    plt.savefig('/home/Thesis/Accuracy Plot.png')